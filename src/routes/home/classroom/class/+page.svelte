3e4d2892b2402f82344bc2c976e7fed20af239fa
jeondohyun0 <tintin3492@gmail.com>
2023-06-25 12:13:21 +0900

    init

---
+++ src/routes/home/classroom/class/+page.svelte
@@ -0,0 +1,64 @@
+<style>
+    .container {
+        margin: auto;
+        width: 600px;
+        height: 630px;
+    }
+    .box-head {
+        background-color: #32BBE7;
+        width: 600px;
+        height: 50px;
+    }
+    table {
+        background-color: #FFFFFF;
+        color: #836666;
+        border: 1px solid #B6A0A0;
+        border-collapse: collapse;
+        width: 600px;
+        text-align: center;
+    }
+    table td {
+            border: 1px solid #B6A0A0;
+            padding: 10px 0;
+            width: 200px;
+    }
+    .box-enter {
+        height: 30px;
+        width: 300px;
+        border-radius: 5px;
+        background-color: #32BBE7;
+        margin: 100px auto;
+    }
+    .text-enter {
+        font-size: 12px;
+        color: #FFFFFF;
+        text-align: center;
+        padding: 7px 0;
+    }
+</style>
+<div class="container">
+    <div class="box-head">
+        <a href="/home">
+            <img src="/classroom/home.png" alt="home" id="home" style="height: 50px; margin: 1px auto">
+        </a>
+    </div>
+    <table>
+        <td style="background-color: #E4DCDC;">수업</td>
+        <td>
+            <a href="/home/classroom/chat" style="text-decoration: none; color: #836666;">
+                <div style="width: auto;">채팅</div>
+            </a>
+        </td>
+        <td>
+            <a href="/home/classroom/manage" style="text-decoration: none; color: #836666;">
+                <div style="width: auto;">수업 관리</div>
+            </a>
+        </td>
+    </table>
+    <div class="box-enter">
+        <div class="text-enter">수업 채널 참가하기</div>
+    </div>
+</div>
+
+
+
