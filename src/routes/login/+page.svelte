ac9cc3781609cdaa6c53fd94e245cd33398b4889
jeondohyun0 <tintin3492@gmail.com>
2023-08-29 17:28:47 +0900

    로그인 기능을 추가함 하지만 오류가 남. canvas1에 그림 그릴 수 있고 화면이 공유 됨. canvas1과 canvas2를 겹치고 canvas2에 그림그릴 수 있음 최종적으로 화면 공유된 상태에서 그림그릴 수 있음

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,50 +1,91 @@
-<script>
-    let state = false;
-    let job = '선택';
-    let tcolor = 'EDF2F7';
-    let scolor = 'EDF2F7';
-    let route = ''
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { 
+        GoogleAuthProvider, //로그인
+        browserSessionPersistence, //로그인 기록 남김
+        getAuth, //로그인 인증 정보 나옴
+        onAuthStateChanged, 
+        setPersistence, //browserSessionPersistence과 같이 쓰임
+        signInWithPopup //어떻게 뜨게 할지
+    } from 'firebase/auth';
+    import type { User } from 'firebase/auth';
+    import {
+        getApps, //들어와 있는 상태인지 파악
+        initializeApp, //앱 만들 때
+        FirebaseError
+    } from 'firebase/app';
+    import type { FirebaseOptions } from 'firebase/app';
+    import type { PageData } from './$types';
+    export let data:PageData;
+    const firebaseConfig = data.firebaseConfig;
+    let curUser:User|null = null;
+    onMount(() => {
+        if(getApps().length === 0){
+            initializeApp(firebaseConfig);
+        }
+        const auth = getAuth();
+        const un = onAuthStateChanged(auth, user => {
+            curUser = user;
+        });
+        return () => {
+            un();
+        }
+    });
 
-    const select = () => {
-        state = !state;
-    }
+    const login = async (firebaseConfig:FirebaseOptions) => {
+        if(getApps().length === 0){
+            initializeApp(firebaseConfig);
+        }
+        const provider = new GoogleAuthProvider();
+        const auth = getAuth();
+        provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+        try{
+            await setPersistence(auth, browserSessionPersistence);
+            const result = await signInWithPopup(auth, provider);
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential?.accessToken;
+            const user = result.user;
+            return { token , user};
+        } catch(error){
+            if(error instanceof FirebaseError){
+                const code = error.code;
+                const message = error.message;
+                // The email of the user's account used.
+                const email = error.customData?.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                console.log({
+                    code, message, email, credential
+                });
+            } else {
+                console.log(error);
+            }
+        }
+    };
 
-
-    const selectTeacher = () => {
-        state = false;
-        job = '선생님'
-        route = 'teacher'
-        tcolor = 'E4E8FF';
-        scolor = 'EDF2F7';
+    const logout = async (firebaseConfig:FirebaseOptions) => {
+        if(getApps().length === 0) {
+            initializeApp(firebaseConfig);
+        }
+        const auth = getAuth();
+        await auth.signOut();
     }
-    const selectStudent = () => {
-        state = false;
-        job = '학생'
-        route = 'student'
-        tcolor = 'EDF2F7';
-        scolor = 'E4E8FF';
-    }
-    
 </script>
 <div class="container">
     <div class="head">로그인</div>
-    <div class="container-select">
-        <div on:click={select} class="box-job" style="background-color: #EDF2F7">
-            <div class="select">{job}</div>
-            <img src="/log/down.png" alt="down" style="width:40px">
-        </div>
-        {#if state}
-        <div class="select-box">
-            <div class="select-teacher" on:click={selectTeacher} style="background-color: #{tcolor};">선생님</div>
-            <div class="select-student" on:click={selectStudent} style="background-color: #{scolor};">학생</div>
-        </div>
-        {/if}
-    </div>
-    <a href="/{route}/home" style="text-decoration: none;">
-        <div class="login" style="margin-top: 50px;">
-            <div class="login-text">로그인</div>
-        </div>
-    </a>
+    
+    {#if curUser}
+    <button on:click={async () => logout(firebaseConfig)}>
+        로그아웃
+    </button>
+        <div>{curUser.displayName}</div>
+        <div>{curUser.phoneNumber}</div>
+        <div>{curUser.email}</div>
+    {:else}
+    <button on:click={() => login(firebaseConfig)}>
+        로그인
+    </button>
+    {/if}
 </div>
 <style>
     .container {
@@ -61,57 +102,17 @@
         margin: auto;
         padding-top: 40px;
     }
-    .box-job {
-        width: 180px;
-        height: 60px;
-        border-radius: 5px;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
-    .select {
-        margin: auto;
-        font-size: 20px;
-        color: #B6A0A0;
-    }
-    .container-select {
-        width: 180px;
-        margin: auto;
-        margin-top: 40px;
-    }
-    .select-box {
-        width: 180px;
-    }
-    .select-teacher {
-        color: #B6A0A0;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        height: 50px;
-        border-top: 1px solid #B6A0A0;
-        font-size: 20px;
-    }
-    .select-student {
-        color: #B6A0A0;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        height: 50px;
-        font-size: 20px;
-    }
-    .login {
+    button {
         background-color: #32BBE7;
         width: 400px;
         margin: auto;
         height: 60px;
         display: flex;
-        justify-content: space-between;
-        align-items: center;
         color: white;
-    }
-    .login-text {
-        margin: auto;
+        border: none;
+        font-size: 18px;
+        margin-top: 50px;
+        align-items: center;
+        justify-content: center;
     }
 </style>
